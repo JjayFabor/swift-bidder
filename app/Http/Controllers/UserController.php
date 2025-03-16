e3f6c5942d916b73aa25a4e8597fdfc353005ac3
JjayFabor <faborjaylordvhan@gmail.com>
2025-03-16 21:59:24 +0800

    Added User Dashboard

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,29 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Services\UserService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use App\Models\Auction;
+use Illuminate\Http\Request;
+use App\Services\UserService;
+use App\Services\AuctionService;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     protected $userService;
+    protected $auctionService;
 
-    public function __construct(UserService $userService)
+    public function __construct(UserService $userService, AuctionService $auctionService)
     {
         $this->userService = $userService;
+        $this->auctionService = $auctionService;
     }
 
     public function index()
     {
+        if (! Auth::check()) {
+            return Inertia::location(route('login'));
+        }
+
         $user = Auth::user();
 
         if (! $user->email_verified_at) {
             return Inertia::location(route('verify.account'));
         }
 
-        return Inertia::render('User/UserDashboard');
+        $auctions = Auction::with(['images'])
+            ->get()
+            ->map(function ($auction) {
+                if ($auction->images->count() === 1) {
+                    $auction->selected_image = $auction->images->first()->image_path;
+                } elseif ($auction->images->count() > 1) {
+                    $auction->selected_image = $auction->images->random()->image_path;
+                } else {
+                    $auction->selected_image = null;
+                }
+
+                return $auction;
+            });
+
+        return Inertia::render('User/UserDashboard', [
+            'auctions' => $auctions,
+        ]);
     }
 
     public function verifyAccount()
